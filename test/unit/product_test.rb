83782e23b227381168d8b41bd39dbe75c374b767
Jorge Maroto <patoroco@gmail.com>
2013-05-24 14:34:30 +0200

    Cap de testing

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
 
   test "product attributes must not be empty" do
     product = Product.new
@@ -36,14 +37,31 @@ class ProductTest < ActiveSupport::TestCase
   end
   
   test "image url" do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c./x/y/z/fred.gif}
-    bad = %{ fred.doc fred.gif/more fred.gif.more }
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c./x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
     
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
     end
     
-    bad.each{ |name| assert new_product(name).valid?, "#{name} shouldn't be valid"}
+    bad.each{ |name| assert new_product(name).invalid?, "#{name} shouldn't be valid"}
   end
-
+  
+  test "product is not valid without a unique title" do
+    product = Product.new(title:  products(:ruby).title,
+                          description: "yyy",
+                          price:  1,
+                          image_url: "fred.gif")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
+  
+  test "product is not valid without a unique title - il18n" do
+    product = Product.new(title:  products(:ruby).title,
+                          description: "yyy",
+                          price:  1,
+                          image_url: "fred.gif")
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join('; ')
+  end
 end
