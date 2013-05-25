24fe1d4af973cbb8e59b5dc8524d01e9b84e410c
Jorge Maroto <patoroco@gmail.com>
2013-05-25 16:17:41 +0200

    Iteration D1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private #se marca como privado el meto, y así se evita que Rails lo
+          #utilice como una acción disponible en el controlador
+
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523164846) do
+ActiveRecord::Schema.define(:version => 20130525134954) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
