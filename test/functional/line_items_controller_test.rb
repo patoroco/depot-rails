1d30d8cc04585687b94620aec55bebb7789c54cf
Jorge Maroto <patoroco@gmail.com>
2013-05-27 18:20:39 +0200

    Iteration E1: sumando productos

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -35,7 +35,7 @@ class LineItemsControllerTest < ActionController::TestCase
   end
 
   test "should update line_item" do
-    put :update, id: @line_item, line_item: { cart: @line_item.cart, product: @line_item.product }
+    put :update, id: @line_item, line_item: { cart_id: @line_item.cart, product_id: @line_item.product }
     assert_redirected_to line_item_path(assigns(:line_item))
   end
 
