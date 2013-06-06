47451b35f8770c5b3956bd1405455ae2701373d2
Jorge Maroto <patoroco@gmail.com>
2013-06-06 19:39:50 +0200

    G1 terminado (checkout)

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,10 @@
 class LineItem < ActiveRecord::Base
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
 
-  attr_accessible :cart_id, :product_id, :quantity
+  attr_accessible :cart_id, :product_id
+  attr_accessible :cart, :product # NOTICE: para pasarselo con el mass-assignment
 
   def total_price
     product.price * quantity
