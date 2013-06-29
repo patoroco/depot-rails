3e6a5fa64ab3347c24aecdc80bb91bd2cccdb0fd
Jorge Maroto <patoroco@gmail.com>
2013-06-29 12:44:04 +0200

    I3 Limiting access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
+
   protect_from_forgery
+
 
   private #se marca como privado el meto, y así se evita que Rails lo
           #utilice como una acción disponible en el controlador
@@ -10,5 +13,12 @@ class ApplicationController < ActionController::Base
       cart = Cart.create
       session[:cart_id] = cart.id
       cart
+    end
+
+  protected
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
     end
 end
